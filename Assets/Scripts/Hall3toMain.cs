e3964ee034a465b2af8b15e24aba75ca0930ab69
Jennifer Li <lilla@live.unc.edu>
2018-04-09 13:46:07 -0400

    Please work
    cool

---
+++ Assets/Scripts/Hall3toMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hall3toMain : MonoBehaviour {
+
+    void onMouseUp()
+    {
+        SceneManager.LoadScene("Mainhall_11");
+    }
+}
---
+++ Assets/Scripts/ToHall1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHall1 : MonoBehaviour {
+
+    void onMouseUp()
+    {
+        SceneManager.LoadScene("Hall1_1");
+    }
+}
