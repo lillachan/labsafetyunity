8acfb08713d064a9948ac8e5325158a79664656c
Jennifer Li <lilla@live.unc.edu>
2018-04-16 20:49:11 -0400

    Transitions Finished
    ****DABS******

---
+++ Assets/Scripts/toHall1_1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toHall1_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Hall1_1");
+    }
+}
