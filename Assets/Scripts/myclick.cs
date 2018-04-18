dbd052f22c03cedd4ab48289754b7bd748b3750b
Sam <shmeulevigdor@gmail.com>
2018-04-18 15:23:43 -0400

    Back Button Working (in hall1_2)
    button mechanics changed. added event system to Panel object (the child of QCanvas).
    see back button script triggers and code for myclick.

--- Assets/Scripts/myclick.cs
+++ Assets/Scripts/myclick.cs
@@ -8,6 +8,8 @@ public class myclick : MonoBehaviour {
 	public GameObject UI;
 	public Text text;
 
+	public GameObject cam;
+
 	// Use this for initialization
 	void Start () {
 		UI = GameObject.FindWithTag ("Menu");
@@ -16,16 +18,36 @@ public class myclick : MonoBehaviour {
 			text = UI.GetComponent<Text> ();
 			Debug.Log (text);
 		}
+		cam = GameObject.FindWithTag ("MainCamera");
 	}
-	
+
+	void DisableMouse() {
+		//Component look = cam.GetComponent<Mouselook> ();
+		//Component ml =
+
+		cam.GetComponent<Mouselook> ().enabled = false;
+		//Camera.current.GetComponent<Mouselook>().enabled = false;
+		//Component looker = 
+//		cam.GetComponent<Mouselook> ().enabled = false;
+
+		//Cursor.lockState = CursorLockMode.None;
+	}
+
+	public void CloseMenu() {
+		UI.SetActive (false);
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	void OnMouseUp() {
 		//transform.localScale += new Vector3(0.1F, 0, 0);
 		//UI.SetActive(!UI.activeInHierarchy);
 		UI.SetActive(true);
+		Cursor.lockState = CursorLockMode.None;
+		DisableMouse ();
 	}
 }
