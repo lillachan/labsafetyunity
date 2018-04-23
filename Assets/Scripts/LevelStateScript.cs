5ec82f010deff633784869d287173661d9a2dc8d
vapaavetehinen <vapaavetehinen@users.noreply.github.com>
2018-04-22 21:38:53 -0400

    refactored questions
    Refactored code for adding questions so questions can be added without sharing to github. Also, added the questions, but this won't show up on github.

--- Assets/Scripts/LevelStateScript.cs
+++ Assets/Scripts/LevelStateScript.cs
@@ -12,7 +12,6 @@ public class LevelStateScript : MonoBehaviour {
 	// Use this for initialization
 	void Awake() {
 		this.InstanceControl();
-        qDict = new Dictionary<string, Question>();
 		populateDict();
 	}
 	private void InstanceControl(){
@@ -28,15 +27,7 @@ public class LevelStateScript : MonoBehaviour {
 
     private void populateDict()
     {
-        //one line per question
-        qDict.Add("fireExtinguisher", new Question(
-                                "Which of the following best describes the safety infraction?",
-                                "answer 1.",
-                                "answer b",
-                                "wrongn answer",
-                                "yes",
-                                "D"
-        ));
+        qDict = LabQuestions.import();
         Debug.Log("test");
     }
     public void updateQ(string id) {
@@ -123,53 +114,6 @@ public class LevelStateScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-	}
-		
-
-}
-
-public class Question {
-	public string qText;
-	public string aText;
-	public string bText;
-	public string cText;
-	public string dText;
-	private string selected;
-	private string correct;
-	public Question(string qText, string aText, string bText, string cText, string dText, string correct){
-		selected = null;
-		this.qText = qText;
-		this.aText = aText;
-		this.bText = bText;
-		this.cText = cText;
-		this.dText = dText;
-		this.correct = correct;
-	}
-	public string currSelected(){
-		return selected;
-	}
-	public void setSelected(string choice){
-		if(choice != selected){
-			selected = choice;
-		}
-		else{
-			selected = null;
-		}
-        Debug.Log(currSelected());
-	}
-	public bool selectedIsCorrect(){
-		if(selected == correct){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	public bool isAnswered(){
-		if(selected != null){
-			return true;
-		}
-		return false;
 	}
 
 }
